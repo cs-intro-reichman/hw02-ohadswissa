9902d04f50317da2bef0a9ee70ac8c367c5ebeed
ohadswissa <ohadswissa217@gmail.com>
2023-12-28 14:13:58 +0200

    last update

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -14,17 +14,6 @@ public class OneOfEachStats {
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		
-		//// In the previous version of this program, you used a statement like:
-		//// double rnd = Math.random();
-		//// Where "rnd" is the variable that stores the generated random value.
-		//// In this version of the program, replace this statement with:
-		//// double rnd = generator.nextDouble();
-		//// This statement will generate a random value in the range [0,1),
-		//// just like you had in the previous version, except that the 
-		//// randomization will be based on the given seed.
-		//// This is the only change that you have to do in the program.
-
 		double sum = 0;
 		int count2 = 0;
 		int count3 = 0;
